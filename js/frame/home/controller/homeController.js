8267ce2184e2fe6bf34b0b55f26c127670c701f6
sunny <1134297156@qq.com>
2016-04-07 11:24:46 +0800

    修改所有页面导航
    ion-nav-bar 替换掉原来的 ion-header-bar

--- js/frame/home/controller/homeController.js
+++ js/frame/home/controller/homeController.js
@@ -1,10 +1,12 @@
 angular.module('app.frame.home')
     .controller('HomeController', function($scope, $ionicBackdrop) {
-        $scope.homePopoverHide = function() {
-            //angular.element('home-title-popover').hide();
+
+        $scope.hidePopover = function() {
             return $scope.homePopover = !$scope.homePopover;
         };
-        //$scope.action = function() {
-        //  $ionicBackdrop.retain();
-        //};
+
+        $scope.showPopover = function (){
+            return $scope.homePopover = !$scope.homePopover;
+        }
+
     })
